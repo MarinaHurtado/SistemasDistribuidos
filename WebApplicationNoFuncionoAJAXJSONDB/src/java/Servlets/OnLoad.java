71b274e0cf774e0c0e146c471186a489f307c3bc
sdist <sdist@itam.mx>
2017-03-24 15:26:12 -0600

    AJAX, JSON, DB

---
+++ WebApplicationNoFuncionoAJAXJSONDB/src/java/Servlets/OnLoad.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author sdist
+ */
+public class OnLoad extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   private Object[][] ResultSetToArray(ResultSet rs) {
+
+        Object data[][]=null;
+
+        try{
+            rs.last();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int numCols = rsmd.getColumnCount();
+            int numRows =rs.getRow();
+            data=new Object[numRows][numCols];
+            int j = 0;
+            rs.beforeFirst();
+            while (rs.next()){
+                for (int i=0;i<numCols;i++){
+                    data[j][i]=rs.getObject(i+1);
+                }
+                j++;
+            }
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
+        return data;
+
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            Connection con =
+            DriverManager.getConnection("jdbc:derby://localhost:1527/DatosPersonas", "", "");
+
+            Statement query = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = query.executeQuery("SELECT * FROM PERSONAS");
+
+            JSONObject jsonResponse = new JSONObject();
+            JSONArray personas = new JSONArray();
+            
+            rs.beforeFirst();
+            while(rs.next()){
+                JSONObject persona = new JSONObject();
+                String nombre = rs.getString("Nombre");
+                out.write(nombre);
+                persona.put("Nombre", nombre);
+                personas.add(persona);
+            }
+            
+            //Object[][] arr = ResultSetToArray(rs);
+      
+//            for (int i=0 ; i<arr.length; i++) {
+//                JSONObject persona = new JSONObject();
+//                persona.put("Nombre", arr[1][2]);
+//                personas.add(persona);
+//            }
+
+            jsonResponse.put("personas", personas);
+            response.setContentType("application/json");
+            System.out.println(jsonResponse.toString());
+            out.write(jsonResponse.toString()); 
+            
+            
+    }   catch (ClassNotFoundException ex) {
+            Logger.getLogger(OnLoad.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(OnLoad.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
